7635e506b75058981cff16a2ae60fa3d82ff2ff9
Master-Bw3 <bw1jochman@gmail.com>
2025-03-06 12:40:00 -0600

    new design

--- src/dice_ability.lua
+++ src/dice_ability.lua
@@ -1,16 +1,17 @@
 DiceAbility = {}
 
+
 SMODS.DrawStep({
     key = "dice_ability", 
-    order = -11, 
+    order = 10, 
     func = function (self)
         if (self.diceAbility) then
-
-            self.diceAbility.sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, 0, 0, -0.35*self.T.w, 0)
+            self.diceAbility.sprite:draw_shader('dissolve', nil, nil, nil, self.children.center)
 
             for i, v in ipairs(self.diceAbility.dice) do
-                v.sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, -0.4, 0, -0.55*self.T.w, 0.3 + (i-1)*0.5)
+                v.sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, 0, 0, 0, 0)
             end
+
         end
     end,
     conditions = { vortex = false, facing = 'front' },
@@ -23,12 +24,13 @@ DiceAbility.ONE = {
     slot_count = 3
 }
 
-function DiceAbility.add_to_card(card, abilty)
-    card.diceAbility = abilty;
-    card.diceAbility.sprite = Sprite(card.T.x, card.T.y, card.T.w, card.T.h, G.ASSET_ATLAS[abilty.atlas], abilty.pos)
+function DiceAbility.add_to_card(card, ability)
+    card.diceAbility = {
+        slot_count = ability.slot_count,
+        sprite = Sprite(card.T.x, card.T.y, card.T.w / 2, card.T.h, G.ASSET_ATLAS[ability.atlas], ability.pos),
+        dice = {}
+    } 
     card.diceAbility.sprite:set_role({major = card, role_type = 'Minor', draw_major = card})
-    card.diceAbility.dice = {}
-
-
+ 
     return card
 end
